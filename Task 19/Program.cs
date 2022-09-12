c38279b73058ed3d91a3b51e8c15f236ed434294
Andrey-Nekrasov-coder <asklshadow8088@gmail.com>
2022-09-12 15:18:51 +0700

    исправил ошибку в 19 задаче!

--- Task 19/Program.cs
+++ Task 19/Program.cs
@@ -1,14 +1,17 @@
 ﻿Console.Write("Введите пятизначное число: ");
 string? number = Console.ReadLine();
 
-void CheckingNumber(string number){
-  if (number[0]==number[4] || number[1]==number[3]){
-    Console.WriteLine($"{number} -> Yes");
-  }
-  else Console.WriteLine($"{number} -> No");
+void CheckingNumber(string number)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} -> Yes");
+    }
+    else Console.WriteLine($"{number} -> No");
 }
 
-if (number!.Length == 5){
-  CheckingNumber(number);
+if (number!.Length == 5)
+{
+    CheckingNumber(number);
 }
 else Console.WriteLine($"Введите правилньое значение!");
