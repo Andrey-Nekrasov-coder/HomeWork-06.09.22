44a24a5dcc24bb1c1b9cb9ad16db3d49f720ebfa
Andrey-Nekrasov-coder <asklshadow8088@gmail.com>
2022-09-12 12:47:45 +0700

    HomeWork 06.09.22

---
+++ Task 19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"{number} -> Yes");
+  }
+  else Console.WriteLine($"{number} -> No");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введите правилньое значение!");
